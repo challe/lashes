5c8b934f85244e1066b335d735715193415e7f5b
challe128 <challe128@gmail.com>
2017-02-21 20:01:40 +0100

    Can not fetch data via API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CustomerService } from './services/customer.service';
+import { Customer } from './models/customer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CustomerService]
 })
-export class AppComponent {
-  title = 'olle test uobbo!';
+export class AppComponent implements OnInit {
+  errorMessage: string;
+  customers: Customer[];
+
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit() { this.getCustomers(); }
+
+  getCustomers() {
+    this.customerService.getCustomers()
+                     .subscribe(customers => this.customers = customers,
+                     error => this.errorMessage = <any>error);
+  }
 }
